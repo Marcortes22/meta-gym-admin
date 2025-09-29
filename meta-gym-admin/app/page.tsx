bc22bf0b26a0afbd21ee88350485be37775ff533
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2025-09-28 21:17:41 -0600

    feat/ supabase configurado exitosamente

--- meta-gym-admin/app/page.tsx
+++ meta-gym-admin/app/page.tsx
@@ -1,9 +1,10 @@
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">Hola</h1>
     </div>
   );
 }
